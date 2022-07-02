e7d586ba48630156e740f2c96978c36d1348cb30
Siddharth Rajput <108334945+Rajputsiddharth@users.noreply.github.com>
2022-07-02 11:41:16 +0000

    1st iteration

---
+++ main.py
@@ -0,0 +1,193 @@
+typeA =["typeA","add","sub","mul","and","or","xor"]
+typeB = ["typeB","mov","ls"]
+typeC = ["typeC","not","cmp","div","mov"]
+typeD = ["typeD","ld","st"]
+typeE = ["typeE","je","jgt","jlt","jmp"]
+typeF = ["typeF","hlt"]
+Instruc = [typeA, typeB, typeC, typeD, typeE, typeF]
+
+#Function to get encoding of register
+def getRegister(reg):
+    dict_reg = {
+        "R0": "000",
+        "R1": "001",
+        "R2": "010",
+        "R3": "011",
+        "R4": "100",
+        "R5": "101",
+        "R6": "110",
+        "FLAGS": "111"
+    }
+
+    return dict_reg[reg]
+
+
+#Function to get encoding of instruction
+def instruction(ins):
+    dict_ins = {
+        "add": "10000",
+        "sub": "10001",
+        "mul": "10110",
+        "div": "10111",
+        "ld": "10100",
+        "st": "10101",
+        "ls": "11001",
+        "rs": "11000",
+        "xor": "11010",
+        "or": "11011",
+        "and": "11100",
+        "not": "11101",
+        "cmp": "11110",
+        "jmp": "11111",
+        "jlt": "01100",
+        "jgt": "01101",
+        "je": "01111",
+        "hlt": "01010",
+        "mov1": "10010",
+        "mov2": "10011"
+    }
+
+    return dict_ins[ins]
+
+
+#function to convert decimal to binary
+
+
+#Function decimal to binary
+def decimal_to_binary(dec):
+    dec = int(dec)
+    bin = ""
+    while dec > 0:
+        bin = str(dec % 2) + bin
+        dec = dec // 2
+
+    while (len(bin) < 8):
+        bin = "0" + bin
+    return bin
+
+
+#function to return type
+
+
+#type of function to return 16 bit encodings
+def Call_TypeA(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append("00")
+    newlist.append(getRegister(list[1]))
+    newlist.append(getRegister(list[2]))
+    newlist.append(getRegister(list[3]))
+    answer = ''.join(newlist)
+    return answer
+
+
+def mov_B(list):
+    newlist = []
+    newlist.append("10010")
+    newlist.append(getRegister(list[1]))
+    newlist.append(decimal_to_binary(list[2][1:]))
+    answer = ''.join(newlist)
+    return answer
+
+
+
+def mov_c(list):
+    newlist = []
+    newlist.append("10011")
+    newlist.append("00000")
+    newlist.append(getRegister(list[1]))
+    newlist.append(getRegister(list[2]))
+    answer = ''.join(newlist)
+    return answer
+
+  
+
+def Call_TypeB(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append(getRegister(list[1]))
+    newlist.append(decimal_to_binary(list[2][1:]))
+    answer = ''.join(newlist)
+    return answer
+
+
+def Call_TypeC(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append("00000")
+    newlist.append(getRegister(list[1]))
+    newlist.append(getRegister(list[2]))
+    answer = ''.join(newlist)
+    return answer
+
+
+def Call_TypeD(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append(getRegister(list[1]))
+    newlist.append(decimal_to_binary(str(count)))
+    answer = ''.join(newlist)
+    return answer
+
+
+def Call_TypeE(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append("000")
+    newlist.append(decimal_to_binary(str(count)))
+    answer = ''.join(newlist)
+    return answer
+
+
+def Call_TypeF(list):
+    newlist = []
+    newlist.append(instruction(list[0]))
+    newlist.append("00000000000")
+    answer = ''.join(newlist)
+    return answer
+
+
+def Call_Func(str, list):
+    if str == "typeA":
+        return Call_TypeA(list)
+    elif str == "typeB":
+        return Call_TypeB(list)
+    elif str == "typeC":
+        return Call_TypeC(list)
+    elif str == "typeD":
+        return Call_TypeD(list)
+    elif str == "typeE":
+        return Call_TypeE(list)
+    elif str == "typeF":
+        return Call_TypeF(list)
+
+
+
+#main function 
+check = 0
+count = 1
+while True:
+    cmd = input("Enter the command : ").split()
+    if cmd[0] == "hlt":
+        print(Call_Func("typeF",cmd))
+        break
+    elif cmd[0] == "mov":
+        if cmd[2][0] == "$":
+            print(mov_B(cmd))
+        else:
+            print(mov_c(cmd))
+    else:
+        for item in Instruc:
+            if cmd[0] in item:
+                print(Call_Func(item[0], cmd))
+                print("\n")
+                check = 1
+                break
+            else:
+                pass
+        # if check==0:     #if the command is not found in the list of instructions
+        #     print("Invalid Instruction")
+    check = 0
+    count += 1
+
+
